21b3c25a393f9586daed6e4b120bbc5d9081bb71
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-20 02:46:43 -0500

    update deps

--- verbfile.js
+++ verbfile.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var verb = require('verb');
-verb.data({foo: 'bar'});
 
-verb.task('readme', function() {
+verb.task('default', function() {
   verb.src('.verb.md')
     .pipe(verb.dest('./'));
 });
-
-verb.task('default', ['readme']);
