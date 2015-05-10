3e646884a8677cace6461960fdda03cff68d0017
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-10 08:30:44 -0400

    lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * template-utils <https://github.com/jonschlinkert/template-utils>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015 Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
--- verbfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var verb = require('verb');
-
-verb.task('default', function() {
-  verb.src('.verb.md')
-    .pipe(verb.dest('./'));
-});
